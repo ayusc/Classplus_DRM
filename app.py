226a892c81d3ffc2f81a21fd42d387ec2759c4f0
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-05-08 00:36:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+import threading
+import time
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Tech VJ'
+def root():
+    return jsonify(status="Running")
+
+@app.route('/health')
+def health():
+    return jsonify(status="Healthy")
+
+def ping_self():
+    while True:
+        try:
+            res = requests.get(f"https:{SITE_URL}/health")
+            if res.status_code != 200:
+                print(f"Self ping failed with status code {res.status_code}")
+            # else:
+            #     print("Self ping succeeded")
+        except Exception as e:
+            print(f"Error in self-ping: {e}")
+        time.sleep(60)
 
 if __name__ == "__main__":
+    threading.Thread(target=ping_self, daemon=True).start()
     app.run(host='0.0.0.0', port=8000)
